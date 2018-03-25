51f7cacaf882d55baead4425c70048f6fc125e4c
ROO Joan Sol <joan-sol.roo@inria.fr>
2018-03-25 22:57:41 +0200

    juicy inventory animation

--- Assets/CopyPosition.cs
+++ Assets/CopyPosition.cs
@@ -5,13 +5,14 @@ using UnityEngine;
 public class CopyPosition : MonoBehaviour {
 
     [SerializeField] Transform other;
-	// Use this for initialization
-	void Start () {
+    [SerializeField] float speed = 1;
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
 	// Update is called once per frame
 	void LateUpdate () {
-        this.transform.position = other.transform.position;
+        this.transform.position = Vector3.MoveTowards(this.transform.position,other.transform.position,speed*Vector3.Distance(this.transform.position, other.transform.position)*Time.deltaTime);
 	}
 }
