27da7e6430b08d82a15f97fb665149760222da5f
ROO Joan Sol <joan-sol.roo@inria.fr>
2018-03-25 22:52:09 +0200

    Inventory over the head

---
+++ Assets/CopyPosition.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CopyPosition : MonoBehaviour {
+
+    [SerializeField] Transform other;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+        this.transform.position = other.transform.position;
+	}
+}
--- Assets/Games/Scripts/TriggerToggle.cs
+++ Assets/Games/Scripts/TriggerToggle.cs
@@ -6,18 +6,35 @@ public class TriggerToggle : MonoBehaviour {
 
     [SerializeField] GameObject visualization;
 
-    float transitionDuration2 = 0.25f;
+    public float transitionDuration2 = 0.25f;
 
+    [SerializeField] bool triggerBased = true; //THIS IS A HACK, these should be 2 different monobehaviours
+    [SerializeField] bool active = true; //THIS IS A HACK, these should be 2 different monobehaviours
     private void Start()
     {
-        HideImmediately();
+        if (!active)
+        {
+            HideImmediately();
+        }
+        wasActive = active;
     }
-    public float dx;
+    bool wasActive = false;
     private void LateUpdate()
     {
+        if (active && !wasActive)
+        {
+            wasActive = active;
+            StartCoroutine("Show");
+        }
+        if (!active && wasActive)
+        {
+            StartCoroutine("Hide");
+            wasActive = active;
+        }
+
         if (visualization.activeSelf)
         {
-            dx = Camera.main.WorldToViewportPoint(this.transform.position).x;
+            float dx = Camera.main.WorldToViewportPoint(this.transform.position).x;
             dx = (dx - 0.5f) * 2;
             Vector3 targetAngle = CameraRotation.instance.transform.eulerAngles + new Vector3(0, dx*-13, 0);
             visualization.transform.rotation = Quaternion.RotateTowards(visualization.transform.rotation, Quaternion.Euler(targetAngle), Quaternion.Angle(visualization.transform.rotation, Quaternion.Euler(targetAngle)) * Time.deltaTime * 5);
@@ -26,12 +43,21 @@ public class TriggerToggle : MonoBehaviour {
     }
     void OnTriggerEnter(Collider other)
     {
-        if(other.gameObject.tag == "Player")
+        if (triggerBased)
         {
-            StartCoroutine("Show");
+            if (other.gameObject.tag == "Player")
+            {
+                active = true;
+            }
         }
     }
-
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            active = false;
+        }
+    }
     float OriginalScale=1;
     void HideImmediately()
     {
@@ -45,7 +71,7 @@ public class TriggerToggle : MonoBehaviour {
     {
         visualization.SetActive(true);
 
-        dx = Camera.main.WorldToViewportPoint(this.transform.position).x;
+        float dx = Camera.main.WorldToViewportPoint(this.transform.position).x;
         dx = (dx - 0.5f) * 2;
         Vector3 targetAngle = CameraRotation.instance.transform.eulerAngles + new Vector3(0, dx * -13, 0);
 
@@ -79,11 +105,5 @@ public class TriggerToggle : MonoBehaviour {
         visualization.SetActive(false);
 
     }
-    void OnTriggerExit(Collider other)
-    {
-        if (other.gameObject.tag == "Player")
-        {
-            StartCoroutine("Hide");
-        }
-    }
+
 }
